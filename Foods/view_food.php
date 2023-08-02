888b8f9d01875839dfb7b382ca115b39ac70597d
Freemano <freemano@MACs-MacBook-Pro.local>
2023-08-02 16:03:58 +0300

    edit

--- Foods/view_food.php
+++ Foods/view_food.php
@@ -25,32 +25,52 @@ include '../connection.php';
 
     }
 </style>
-<nav style="background-color: #ddd;" class="navbar navbar-expand-lg ">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Aarons and sons invest</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="food.php">Foods</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admindashboard.php">Dashboard</a>
-                </li> <li class="nav-item">
-                    <a class="nav-link" href="reviews.php">Reviews</a>
-                </li>
-                </li> <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
+
+<style>
+    .head{
+        background: rgba(10,10,10,1);
+        color: white;
+        width: 100%;
+        z-index: 1;
+    }
+    @media screen and (max-width: 500px) and (min-width: 200px) {
+        .head{
+            background: rgba(10,10,10,1);
+            color: white;
+            width: 100%;
+            padding: 1rem;
+            z-index: 1;
+        }
+    }
+
+    ul li:hover{
+        padding-left: 1rem;
+        background: rgba(23,87,255,0.4);
+    }
+</style>
+<div id="main_nav" class="d-flex sticky-top head container-fluid justify-content-between align-items-center">
+    <a class="navbar-brand "  href="index.php">Aarons and sons invest</a>
+    <ul id="navbar" class="d-md-flex d-lg-flex d-none  list-unstyled">
+        <li class="nav-item mx-3 mt-3 px-2 py-2">
+            <a class="nav-link active" aria-current="page" href="../food.php">Foods</a>
+        </li>
+
+        <li class="nav-item mx-3 mt-3 px-2 py-2">
+            <a class="nav-link" href="./../reviews.php">Reviews</a>
+        </li>
+        <li class="nav-item mx-3 mt-3 px-2 py-2">
+            <a class="nav-link" href="../dashboard.php">Dashboard</a>
+        </li>
+        </li>
+        <li class="nav-item mx-3 mt-3 px-2 py-2">
+            <a class="nav-link" href="../logout.php">Logout</a>
+        </li>
+    </ul>
+    <div style="position: absolute;top:1.1rem;right: 1rem;" id="show" style="font-size: 29px;"> <i class="bi d-sm-block d-md-none d-lg-none bi-list"></i></div>
 
 
-            </ul>
-
-        </div>
-    </div>
-</nav>
+</div>
+</div>
 <div class="main_content">
 
     <?php
@@ -66,7 +86,7 @@ include '../connection.php';
     ?>
     <table class="table m-2 w-100  px-1 table-responsive-sm table-primary table-hover table-bordered">
         <thead>
-        <tr><td class="text-center text-uppercase" colspan="5"> Active Orders</td></tr>
+        <tr><td class="text-center text-uppercase" colspan="6"> Active Orders</td></tr>
         <tr>
             <th>Name</th>
             <th>Food Image</th>
@@ -110,6 +130,16 @@ include '../connection.php';
 
 </div>
 
+<script>
+    const main_nav = document.getElementById('main_nav')
+    const show = document.getElementById('show')
+    const navbar = document.getElementById('navbar')
+    show.addEventListener('click',()=>{
+        navbar.classList.toggle('d-none')
+        main_nav.classList.toggle('flex-column')
+        main_nav.classList.toggle('align-items-center')
+    })
+</script>
 </body>
 </html>
 
